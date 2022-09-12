45ae46e1cb81d06d8d4737946f7a64a6a14cadd3
Alexey <aleksejmalgin1@g.mail.com>
2022-09-13 01:09:53 +0500

    Add задача 6

---
+++ задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 1)
+    {
+     Console.WriteLine("Число " + n + " является: НЕЧЁТНЫМ");
+    }
+else
+    {
+    Console.WriteLine("Число " + n + " является: ЧЁТНЫМ");
+    }
+    
