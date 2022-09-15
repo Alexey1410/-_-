3ed19c02fd0dadce76d9074bcd84e00d6b08d5c6
Alexey <aleksejmalgin1@g.mail.com>
2022-09-15 17:28:47 +0500

    Added Задача8 git commit -m Added

---
+++ Задача8/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 1;
+bool not =true;
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные число от 1 до " + n);
+while (i <= n)
+{
+    if (i % 2 != 1 )
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет четных чисел.");
+}
