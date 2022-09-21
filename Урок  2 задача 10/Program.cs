ca3b05e42bcce40a9311f3f72644abe43622a071
Alexey <aleksejmalgin1@g.mail.com>
2022-09-22 00:54:44 +0500

    added Задача 10

---
+++ Урок  2 задача 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+int GenerateRandomNamber()
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+void CompraiseNambers()
+{
+    int n = GenerateRandomNamber();
+    Console.WriteLine(n);
+    Console.WriteLine(Convert.ToString(n)[1]);
+}
+
+CompraiseNambers();
